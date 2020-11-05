90c531743f299e102ee315d9db00aa1297224fb3
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-11-04 21:04:41 -0300

    return button

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import "../assets/styles/components/Player.scss";
 
 const BACK = "Regresar";
+
 const Player = props => {
+    const {id}=props.match.params;
     return (
         <div className="Player">
             <video controls autoplay>
                 <source src="" type="video/mp4" />
             </video>
-            <div className="Player-black">
-                <button type="button">
+            <div className="Player-back">
+                <button type="button" onClick={() => props.history.goBack()} >
                     {BACK}
                 </button>
             </div>
-        </div>
+        </div >
     )
 };
 export default Player;
